af9ea42ce6332f58384d24e48453b96c6406ce7d
sdmantha <sdangelcake@gmail.com>
2023-02-09 10:00:14 -0500

    updated settings.py

--- group_alpha_backend/settings.py
+++ group_alpha_backend/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'group_alpha_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'food',
+        'USER': 'group_alpha_backend_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
