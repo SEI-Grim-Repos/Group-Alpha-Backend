431ee89af1bf24612694e0e0465cf39640052264
individual-ism <individual-ism-code@proton.me>
2023-02-13 16:23:42 -0500

    CORS brute force

--- group_alpha_backend/settings.py
+++ group_alpha_backend/settings.py
@@ -102,7 +102,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'group_alpha_backend.wsgi.application'
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', 'gourmet-gather.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
