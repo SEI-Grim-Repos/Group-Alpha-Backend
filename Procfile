62eccdf71e2007da3b14218fc4bc6127d639f0bf
individual-ism <individual-ism-code@proton.me>
2023-02-15 00:17:18 -0500

    Revert "Test"

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn group_alpha_backend.wsgi --log-file -
--- foodies/views.py
+++ foodies/views.py
@@ -1,17 +1,16 @@
 from rest_framework import viewsets
 
-from .serializers import PostSerializer, UserSerializer, CommentSerializer
-from .models import Post, User, Comment
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+from .serializers import UserSerializer, CommentSerializer, PostSerializer
+from .models import User, Comment, Post
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+    
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
