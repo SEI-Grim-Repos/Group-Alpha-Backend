29b113635fc07cae1a918868cf9f7197dd31b911
individual-ism <individual-ism-code@proton.me>
2023-02-15 22:19:55 -0500

    Updates dependencies

--- group_alpha_backend/urls.py
+++ group_alpha_backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import routers
-from foodies.views import PostViewSet, CommentViewSet
+from foodies.views import PostViewSet, CommentViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'Post', PostViewSet)
