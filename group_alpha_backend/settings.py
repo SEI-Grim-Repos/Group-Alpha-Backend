f23605b8a97df994bb6ae2bf0285782e8d73cf93
individual-ism <individual-ism-code@proton.me>
2023-02-15 22:12:38 -0500

    Updates database information

--- group_alpha_backend/settings.py
+++ group_alpha_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o%9gyf9*lpcs*_tbb*xdw&t+-&st2sf$oat&embvex-os0)+_b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://gg1114.herokuapp.com', 'gg1114.herokuapp.com', 'localhost', 'http://localhost']
+ALLOWED_HOSTS = ['https://gourmet-gather.herokuapp.com', 'gourmet-gather.herokuapp.com', 'localhost', 'http://localhost']
 
 
 # Application definition
@@ -100,10 +100,10 @@ WSGI_APPLICATION = 'group_alpha_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd7ccfvpjnn3nbm',
-        'USER': 'sozjnewxceuitp',
-        'PASSWORD': '631977fbc2d4aa1a93cabf08e51e0bb9e2c7562f575a3468ab7f206ea7a45607',
-        'HOST': 'ec2-54-164-40-66.compute-1.amazonaws.com',
+        'NAME': 'dfmb5tm53tc596',
+        'USER': 'gggcgsvhmtlumt',
+        'PASSWORD': '23b098222858ab58cac731d7c2326da62fda29cbdfdf6eb516e2f331c7b250cd',
+        'HOST': 'ec2-44-199-143-43.compute-1.amazonaws.com',
     }
 }
 
@@ -152,7 +152,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://gg1114.herokuapp.com',
+    'https://gourmet-gather.herokuapp.com',
     'http://localhost',
 ]
 
