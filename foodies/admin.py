b7f810880a9edcda29c84551afba8ebda66ba142
individual-ism <individual-ism-code@proton.me>
2023-02-16 16:28:23 -0500

    WE HAVE AUTHENTICATION AT LAST

--- foodies/admin.py
+++ foodies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User_Account, Post, Comment
 
 # Register your models here.
+
+admin.site.register(User_Account)
+admin.site.register(Post)
+admin.site.register(Comment)
--- foodies/serializers.py
+++ foodies/serializers.py
@@ -1,18 +1,40 @@
 from rest_framework import serializers
     
-from .models import Post, User, Comment
+from .models import Post, User_Account, Comment
+
+class User_AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_Account
+        fields = '__all__'
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
         
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+class PostSerializer(serializers.BaseSerializer):
+  def to_representation(self,instance):
+    return {
+      "id": instance.id,
+      "body": instance.body,
+      "user": instance.user.username,
+      "location": instance.location,
+      "date": instance.date,
+      "title": instance.title,
+      "image": instance.image,
+      "likes": instance.likes,
+    }
+    
+class CommentSerializer(serializers.BaseSerializer):
+    def to_representation(self,instance):
+      return {
+      "id":instance.id,
+      "body": instance.body,
+      "user": instance.user.name,
+    }
